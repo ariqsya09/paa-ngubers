583240a8e9dff515167b619935b92ee625acb929
Fikri Ariqsya <fikriariqsyarahman98@gmail.com>
2022-05-22 20:16:56 +0700

    Add halaman SignUp

---
+++ src/SignUp/SignUp.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import {
+  Flex,
+  Box,
+  Heading,
+  FormControl,
+  FormLabel,
+  Input,
+  Button
+} from '@chakra-ui/react';
+import { SimpleGrid } from '@chakra-ui/react'
+import { Image } from '@chakra-ui/react'
+import bike from './logo-bike.png';
+import "./SignUp.css";
+
+export default function SignUp() {
+  // const [input, setInput] = useState('')
+    return (
+    <SimpleGrid columns={2} spacing={10}>
+        <Box>
+        <Flex width="full" align="center" justifyContent="center">
+        <Box p={16}>
+          <Box textAlign="center">
+            <Heading>Sign Up</Heading>
+            <h1>Gak Punya Akun? Yuk Daftar</h1>
+          </Box>
+          <Box my={16} textAlign="left">
+            <form>
+              <FormControl isRequired>
+                <Input type="email" placeholder="Email" />
+              </FormControl>
+              <FormControl mt={3} isRequired>
+                <Input type="text" placeholder="No. Telepon" />
+              </FormControl>
+              <FormControl mt={3} isRequired>
+                <Input type="password" placeholder="Password" />
+              </FormControl>
+              <FormControl mt={3} isRequired>
+                <Input type="password" placeholder="Re-Enter Password" />
+              </FormControl>
+              <Button width="full" mt={4} type="submit" colorScheme="green">
+                Sign Up
+              </Button>
+            </form>
+          </Box>
+        </Box>
+      </Flex>
+        </Box>
+        <Box >
+
+            <Image m={20} boxSize='300px' src={bike}  />
+
+        </Box>
+    </SimpleGrid>
+
+    );
+
+  }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import SignUp from './SignUp/SignUp';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -14,6 +15,7 @@ root.render(
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<App />} />
+          <Route path='/SignUp' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
